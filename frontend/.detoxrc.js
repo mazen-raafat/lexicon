e278b6ca486d5c95cfa56071c7bbe43e8c98fb98
Mazen Raafat <m.a.z.e.n@outlook.com>
2024-10-29 13:05:51 +0300

    v2: change app name and identifier to lexicon

--- frontend/.detoxrc.js
+++ frontend/.detoxrc.js
@@ -14,7 +14,7 @@ module.exports = {
       type: 'ios.app',
       binaryPath: 'bin/IOS/kc.app',
       build:
-        'xcodebuild -workspace ios/lexicon.xcworkspace -scheme kc -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build | npx excpretty ./',
+        'xcodebuild -workspace ios/lexicon.xcworkspace -scheme lexicon -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build | npx excpretty ./',
     },
     'android.debug': {
       type: 'android.apk',
